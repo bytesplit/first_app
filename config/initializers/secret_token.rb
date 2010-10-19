9bda394060813be9867e448e9f30e3c0f7702027
Bytesplit <bytesplit@users.noreply.github.com>
2010-10-19 14:45:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b743dd8ee13b0adb2def8e2dffa31c3225264c0b108c2692e4cd504cf6b6dd87941923311036df607ae603ff5564ece1b17eeab81974b2faedbf6b12d07d248d'
